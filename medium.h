8055c440aa180c525826733dbaae66588a656cc4
Ian <pyropter28@gmail.com>
2014-07-03 10:24:58 -0500

    Removed SDL_Window (which is exclusive to SDL2) and changed methods so that they function now

--- medium.h
+++ medium.h
@@ -2,6 +2,7 @@
 #define MEDIUM_H
 
 #include "SDL/SDL.h"
+#include <string>
 
 namespace SDL {
 
@@ -47,21 +48,25 @@ namespace SDL {
     struct Rect {
         SDL_Rect* self = NULL;
         Rect(int x1, int y1, int x2, int y2) {
-            this->self = new SDL_Rect(x1, y1, x2-x1, y2-y1);
+            this->self = new SDL_Rect();
+            self->x = x1;
+            self->y = y1;
+            self->w = x2-x1;
+            self->h = y2-y1;
         }
-    }
+    };
 
     struct Image {
         SDL_Surface* self = NULL;
-        Image(SDL_Surface* sf) {
-            this->sf = sf;
+        Image(SDL_Surface* self) {
+            this->self = self;
         }
     };
     
-    struct Window {
-        SDL_Window* win = NULL;
-        Window(SDL_Window* win) {
-            this->win = win;
+    struct VideoMode {
+        SDL_Surface* self = NULL;
+        VideoMode(int width, int height, int bpp) {
+            this->self = SDL_SetVideoMode(width, height, bpp, SDL_SWSURFACE);
         }
     };
 
@@ -71,21 +76,45 @@ namespace SDL {
     }
     
     void destroy(Image* img) {
-        SDL_FreeSurface(img->sf);
+        SDL_FreeSurface(img->self);
         img = NULL;
-    }
-
-    void destroy(Window* win) {
-        SDL_DestroyWindow(win->win);
-        win = NULL;
     }
     
     void draw(Image* src, Image* dest) {
         SDL_BlitSurface(src->self, NULL, dest->self, NULL);
     }
     
-    SDL_Surface* getBMP(const char* filename) {
-        return SDL_DisplayFormat(SDL_LoadBMP(filename));
+    void draw(Image* src, VideoMode* dest) {
+        SDL_BlitSurface(src->self, NULL, dest->self, NULL);
+    }
+    
+    Image* load(const char* filename) {
+        enum { BMP, JPG, PNG };
+        int type;
+        std::string str(filename);
+        
+        if      (str.find(".bmp") != std::string::npos)
+            type = BMP;
+        else if (str.find(".jpg") != std::string::npos)
+            type = JPG;
+        else if (str.find(".png") != std::string::npos)
+            type = PNG;
+        
+        SDL_Surface *loaded, *optimized;
+        
+        switch (type) {
+            case BMP: loaded = SDL_LoadBMP(filename); break;
+            default:  loaded = NULL;
+        }
+        
+        optimized = SDL_DisplayFormat(loaded);
+        SDL_FreeSurface(loaded);
+        
+        return new Image(optimized);
+    }
+    
+    void init() {
+        SDL_Init(SDL_INIT_EVERYTHING);
     }
     
     void quit() {
@@ -97,12 +126,11 @@ namespace SDL {
     }
     
     void update(Image* img) {
-        SDL_Flip(img->sf);
+        SDL_Flip(img->self);
     }
     
-    Window* window(const char* text, int width, int height) {
-        return new Window(SDL_CreateWindow(text, SDL_WINDOWPOS_CENTERED, 
-                          SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN));
+    void update(VideoMode* vm) {
+        SDL_Flip(vm->self);
     }
 }
 
